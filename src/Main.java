39b639bcd92f1a02c7e42de877a0b05ef10a7d85
Karimova Dina <dinaidina@mail.ru>
2022-08-22 07:17:34 +0300

    Initial repository

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 5600; // стоимость авиабилета
+        int oneMile = 20; // начисляется милль за 1 рубль
+
+        int mile = price/oneMile; // начислится милль за билет
+
+        System.out.println(mile + "милль");
+
+    }
+}
